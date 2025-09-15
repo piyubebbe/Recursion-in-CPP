e1172e13ca1264fb29bdbe10366c569d079e8dff
Piyush <118040098+piyubebbe@users.noreply.github.com>
2025-09-15 15:36:10 +0530

    Add files via upload

---
+++ EXP_15_b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    if (n <= 1)
+        return 1;
+    else
+        return n + sum(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number here: ";
+    cin >> n;
+    cout << "Sum upto" << "n" << " = " << sum(n);
+
+    return 0;
+}
+
+/*
+Output:
+Enter a number here: 5
+Sum upto 5 = 15
+*/
---
+++ EXP_15_c.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void revStr(char *str)
+{
+    if(*str != '\0')
+    {
+        revStr(str+1);
+        cout << *str;
+    }
+}
+
+int main()
+{
+    char n[100];
+    cout << "Enter a string here: ";
+    cin >> n;
+    cout << "Reverse string = ";;
+    revStr(n);
+
+    return 0;
+}
+
+/*
+Output:
+Enter a string here: hello
+Reverse string = olleh
+*/
